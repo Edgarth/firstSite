e8c191d751bd3cfb529f43f449258dc3daf3193c
Arthur <arthur.heynard@concoursmania.com>
2013-01-25 09:55:21 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstSite::Application.config.secret_token = '9839020c3183be401e3bde097a9336627f38ebed6ddfac4b12ace875c0473b23652d5ee73fa9ebb95c0cdda7995ae400fc0a66b33cd2a50ea0d15303e1627e9b'
